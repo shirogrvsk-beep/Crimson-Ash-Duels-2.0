7ee24124cdf2ce519f9c6b74fa2081fb8c3664d0
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-26 01:08:47 +0800

    DuelCommand.java

---
+++ src/main/java/com/crimsonashduels/commands/DuelCommand.java
@@ -0,0 +1,8 @@
+package com.crimsonashduels.commands;
+
+import com.crimsonashduels.DuelManager;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+
+public class Duel
