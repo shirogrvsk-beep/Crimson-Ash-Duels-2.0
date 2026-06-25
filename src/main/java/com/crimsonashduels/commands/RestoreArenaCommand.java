714c2e5d19bf5536d47b2f4942cd64275dfc84fb
shirogrvsk-beep <shirogrvsk@gmail.com>
2026-06-26 01:17:31 +0800

    RestoreArenaCommand.java

--- src/main/java/com/crimsonashduels/commands/RestoreArenaCommand.java
+++ src/main/java/com/crimsonashduels/commands/RestoreArenaCommand.java
@@ -1,7 +1,9 @@
 package com.crimsonashduels.commands;
 
 import com.crimsonashduels.CrimsonAshDuels;
+import com.crimsonashduels.ArenaResetManager;
 import org.bukkit.command.*;
+import org.bukkit.entity.Player;
 
 public class RestoreArenaCommand implements CommandExecutor {
     private final CrimsonAshDuels plugin;
@@ -12,7 +14,19 @@ public class RestoreArenaCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("Arena restore not implemented yet.");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can restore arenas.");
+            return true;
+        }
+        if (args.length < 1) {
+            sender.sendMessage("Usage: /restorearena <name>");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        ArenaResetManager manager = plugin.getArenaResetManager();
+        manager.restoreArena(args[0], player.getLocation());
+        player.sendMessage("Arena " + args[0] + " restored at your location.");
         return true;
     }
 }
